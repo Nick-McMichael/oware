9c904c473cd0fcaeb4913cd5025ecc8fba87d2cb
William Nel <G17N9007@ict.ru.ac.za>
2019-03-19 20:32:07 +0200

    Board and structured types done

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,8 +1,37 @@
 ﻿module Oware
 
+open System.Drawing
+
 type StartingPosition =
     | South
     | North
+
+type GameState =
+| SouthTurn
+| NorthTurn
+| GameEndedDraw
+| SouthWon
+| NorthWon
+
+let printState = function
+| SouthTurn-> "South’s turn"
+| NorthTurn -> "North's turn"
+| GameEndedDraw -> "Game ended in a draw"
+| SouthWon -> "South won"
+| NorthWon -> "North won"
+| _ -> failwith "Exception: Invalid Game State!"
+
+type Player = {
+    Houses : int * int * int * int * int * int
+    Name : StartingPosition
+    CapturedSeeds : int
+}
+
+type Board = {
+    P1 : Player
+    P2 : Player
+    State : GameState
+}
 
 let getSeeds n board = failwith "Not implemented"
 
